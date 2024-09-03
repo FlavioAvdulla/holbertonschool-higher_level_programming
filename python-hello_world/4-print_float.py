c09b3729fe7931d6f175e051c2035f491e7d5ca8
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-03 11:58:11 +0000

    9-easter_egg.py

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -4,13 +4,13 @@ string = input('Float:')
 try:
     number = float(string)
     if number > 0:
-        print("{:.2f}\n".format(number))
+        print("{:.2f}".format(number))
         print("Correct output - positive")
     elif number < 0:
-        print("{:.2f}\n".format(number))
+        print("{:.2f}".format(number))
         print("Correct output - negative")
     else:
-        print("{:.2f}\n".format(number))
+        print("{:.2f}".format(number))
         print("Correct output - zero")
 except ValueError:
     print("Correct output - wrong type")
