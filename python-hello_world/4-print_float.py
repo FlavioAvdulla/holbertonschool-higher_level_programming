f70c851a7830b8f3cafcc429a3570da9b18ab9fc
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-03 12:09:16 +0000

    4-print_float.py

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,17 +1,3 @@
 #!/usr/bin/python3
 number = 3.14159
-string = input('Float:')
-
-try:
-    number = float(string)
-    if number > 0:
-        print("{:.2f}".format(number))
-        print("Correct output - positive")
-    elif number < 0:
-        print("{:.2f}".format(number))
-        print("Correct output - negative")
-    else:
-        print("{:.2f}".format(number))
-        print("Correct output - zero")
-except ValueError:
-    print("Correct output - wrong type")
+print(f"Float: {number:.2f}")
