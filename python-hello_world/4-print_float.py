9a179271f7c61f8c4dafc49bcaf305346eb0f777
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-03 10:26:02 +0000

    2-print.py

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 string = input('Float:')
+
 try:
-	number = float(string)
-	if number > 0:
-		print("{:.2f}\n".format(number))
-		print("Correct Output - positive")
-	elif number < 0:
-		print("{:.2f}\n".format(number))
-		print("Correct Output - negative")
-	else:
-		print("{:.2f}\n".format(number))
-		print("Correct Output - zero")
+    number = float(string)
+    if number > 0:
+        print("{:.2f}\n".format(number))
+        print("Correct output - positive")
+    elif number < 0:
+        print("{:.2f}\n".format(number))
+        print("Correct output - negative")
+    else:
+        print("{:.2f}\n".format(number))
+        print("Correct output - zero")
 except ValueError:
-	printf("Correct Output - wrong type")
+    print("Correct output - wrong type")
