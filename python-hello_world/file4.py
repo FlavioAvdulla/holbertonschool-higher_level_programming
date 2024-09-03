2996d2e69e70e77f77a635587d95fcb56d09695c
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-03 08:14:44 +0000

    upadate

---
+++ python-hello_world/file4.py
@@ -0,0 +1,15 @@
+# We'll provide different output based on age
+# 1 - 18 -> Important
+# 21, 50, > 65 -> Important
+# All others -> Not Important
+
+# Receive age and store in age
+
+
+# If age is both greater than or equal to 1 and less than or equal to 18 Important
+
+
+# If age is either 21 or 50 Important
+
+
+# We check if age is less than 65 and then convert 
