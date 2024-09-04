2b45cdbe51c60ba06d777dd45cc0cb28b6d9116c
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-04 13:02:00 +0000

    2-print_alphabet.py

--- python-if_else_loops_functions/file4.py
+++ python-if_else_loops_functions/file4.py
@@ -1,11 +1,25 @@
-#!/usr/bin/python3
-import random
-number = random.randint(-1, 1)
-if number > 0:
-    print(f"{number} is positive")
+# def check_number(number):
+#     if not isinstance(number, int):
+#         print("Please enter a valid integer.")
+#         return
+    
+#     if number % 2 == 0:
+#         print(f"{number} is an even number.")
+#     else:
+#         print(f"{number} is an odd number.")
 
-if number == 0:
-    print(f"{number} is zero")
 
-if number < 0:
-    print(f"{number} is negative")
+# check_number(15)
+# check_number(20)
+# check_number("a string")
+
+
+
+
+
+
+def print_numbers(n):
+    for n in range(1, n + 1):
+        print(n)
+
+print_numbers(10)
