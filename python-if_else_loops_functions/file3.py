7388c530a6c1d3f59267c120e8ff3a3781c622bd
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-04 09:20:48 +0000

    1-last_digit.py

---
+++ python-if_else_loops_functions/file3.py
@@ -0,0 +1,20 @@
+num1, operator, num2 = input("Enter calculation: ").split()
+
+num1 = int(num1)
+num2 = int(num2)
+
+if operator == "+":
+    print(f"{num1} + {num2} = {num1 + num2}")
+
+elif operator == "*":
+    print(f"{num1} * {num2} = {num1 * num2}")
+
+elif operator == "/":
+    print(f"{num1} / {num2} = {num1 / num2}")
+
+elif operator == "-":
+    print(f"{num1} - {num2} = {num1 - num2}")
+
+
+else:
+    print("Invalid output")
