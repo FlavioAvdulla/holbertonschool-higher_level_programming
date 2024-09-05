9a70736d5216bf26cb6f7793e59b8ee03dcc1645
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-05 10:29:59 +0000

    10-add.py

--- python-if_else_loops_functions/10-add.py
+++ python-if_else_loops_functions/10-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def add(a, b):
+    if not isinstance(a, int) or not isinstance(b, int):
+        return "Error: Both arguments must be integers"
     return a + b
 
 add = __import__('10-add').add
@@ -7,3 +9,6 @@ add = __import__('10-add').add
 print(add(1, 2))
 print(add(98, 0))
 print(add(100, -2))
+print(add(-100, -2))
+print(add(0, 0))
+print(add(98, "Holberton"))
