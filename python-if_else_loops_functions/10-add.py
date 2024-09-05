124ffcbd52cb562313ad78b78a08ade33f27c1b2
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-05 11:33:46 +0000

    10-add.py

--- python-if_else_loops_functions/10-add.py
+++ python-if_else_loops_functions/10-add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def add(a, b):
-    if type(a) is not int:
-        a = 0
-    if type(b) is not int:
-        b = 0
+    if type(a) is not int or type(b) is not int:
+        raise TypeError("Both arguments must be integers")
     return a + b
