f3edc9e24d3eb79971167fcdba609ea1a956d40d
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-05 08:24:53 +0000

    8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def uppercase(str):
+    result = ""
     for char in str:
         if 'a' <= char <= 'z':
-            print("{:c}".format(ord(char) - 32), end = "")
+            result += "{:c}".format(ord(char) - 32)
         else:
-            print("{:c}".format(ord(char)), end="")
+            result += "{:c}".format(ord(char))
+    print(result)
 
 uppercase("best")
 uppercase("Best School 98 Battery street")
