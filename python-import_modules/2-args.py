6d830ef5a457f6318c3d4a1861338e6e17a0b4d3
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-07 08:21:18 +0000

    2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+def main():
+    argv = sys.argv
+    argc = len(argv) - 1
+
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 arguments:")
+    else:
+        print("{} arguments:".format(argc))
+
+    for i in range(1, argc + 1):
+        print("{}: {}".format(i, argv[i]))
+
+if __name__ == "__main__":
+    main()
