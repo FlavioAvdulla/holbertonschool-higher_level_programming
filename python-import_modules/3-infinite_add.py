83d2dbf1e17b18c93a49e22014cb938a5ecfd7e5
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-07 08:31:37 +0000

    3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 import sys
 
-
 def main():
-    argv = sys.srgv[1:]
+    argv = sys.argv[1:]
     total = sum(int(arg) for arg in argv)
     print(total)
 
 if __name__ == "__main__":
     main()
-    
