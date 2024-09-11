f7b2502dd9d4cfbf40931f55e836bf22672b9228
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-11 11:32:57 +0000

    file1.py

--- file1.py
+++ file1.py
@@ -24,16 +24,53 @@
 # print(uppercase_names_lc(["John", "James", "Alice"]))
 
 
-def filter_even_numbers(numbers):
-    even_numbers = []
-    for x in numbers:
-        if x in numbers:
-            if x % 2 == 0:
-                even_numbers.append(x)
-    return even_numbers
+# def filter_even_numbers(numbers):
+#     even_numbers = []
+#     for x in numbers:
+#         if x in numbers:
+#             if x % 2 == 0:
+#                 even_numbers.append(x)
+#     return even_numbers
 
-def filter_even_numbers_lc(numbers):
-    return["Even" if x % 2 == 0 else "Odd" for x in numbers ]
+# def filter_even_numbers_lc(numbers):
+#     return["Even" if x % 2 == 0 else "Odd" for x in numbers]
 
-print(filter_even_numbers([1, 2, 3, 4, 5]))
-print(filter_even_numbers_lc([1, 2, 3, 4, 5]))
+# print(filter_even_numbers([1, 2, 3, 4, 5]))
+# print(filter_even_numbers_lc([1, 2, 3, 4, 5]))
+
+
+# def filter_palindromic_words(words):
+#     palindromic_words = []
+#     for word in words:
+#         if word == word[::-1]:
+#             palindromic_words.append(word)
+#     return palindromic_words
+
+# def filter_palindromic_words_lc(words):
+#     return [word for word in words if word == word[::-1]]
+
+# print(filter_palindromic_words(["adam", "madam", "dam"]))
+# print(filter_palindromic_words_lc(["adam", "madam", "dam"]))
+
+
+# set1 = {1, 2, 3}
+# set2 = {3, 4, 5}
+# union_set = set1 | set2
+# print(union_set)
+
+
+# set1 = {1, 2, 3}
+# set2 = {3, 4, 5}
+# union_set = set1 & set2
+# print(union_set)
+
+
+# set1 = {1, 2, 3}
+# set2 = {3, 4, 5}
+# union_set = set1 - set2
+# print(union_set)
+
+# set1 = {1, 2, 3}
+# set2 = {3, 4, 5}
+# union_set = set1 ^ set2
+# print(union_set)
