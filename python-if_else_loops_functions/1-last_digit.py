0c8dd2aa9d50a40433536e439bbaae233f1ca8b5
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-11 07:10:51 +0000

    10-best_score.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,60 @@
 #!/usr/bin/python3
 import random
+
+"""
+Script Documentation
+
+Overview:
+This Python script generates a random integer between -10,000 and 10,000, calculates its last digit, and prints a message based on the value of the last digit.
+
+Modules Used:
+- random: This module is used to generate random numbers.
+
+Variables:
+- text: A string that is part of the message to be printed.
+- number: A randomly generated integer between -10,000 and 10,000.
+- last_digit: The last digit of the absolute value of 'number'.
+
+Logic:
+1. Generate a Random Number:
+   number = random.randint(-10000, 10000)
+   This line generates a random integer between -10,000 and 10,000.
+
+2. Calculate the Last Digit:
+   last_digit = abs(number) % 10
+   This line calculates the last digit of the absolute value of 'number'.
+
+3. Adjust Last Digit for Negative Numbers:
+   if number < 0:
+       last_digit = last_digit * -1
+   If the original number is negative, the last digit is made negative.
+
+4. Print Messages Based on Last Digit:
+   - If the last digit is greater than 5:
+     if last_digit > 5:
+         print(f"{text} {number} is {last_digit} and is greater than 5")
+   - If the last digit is 0:
+     if last_digit == 0:
+         print(f"{text} {number} is {last_digit} and is 0")
+   - If the last digit is less than 6 and not 0:
+     if last_digit != 0 and last_digit < 6:
+         print(f"{text} {number} is {last_digit} and is less than 6 and not 0")
+"""
+
+# A string that is part of the message to be printed
 text = 'Last digit of'
+
+# Generate a random integer between -10,000 and 10,000
 number = random.randint(-10000, 10000)
+
+# Calculate the last digit of the absolute value of 'number'
 last_digit = abs(number) % 10
 
+# If the original number is negative, make the last digit negative
 if number < 0:
     last_digit = last_digit * -1
 
+# Print messages based on the value of the last digit
 if last_digit > 5:
     print(f"{text} {number} is {last_digit} and is greater than 5")
 
--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,13 +1,43 @@
 #!/usr/bin/python3
+
+"""
+Script Documentation
+
+Overview:
+This Python script performs basic arithmetic operations (addition, subtraction, multiplication, and division) using functions imported from the `calculator_1` module. It then prints the results of these operations.
+
+Modules Used:
+- calculator_1: This module contains the arithmetic functions `add`, `sub`, `mul`, and `div`.
+
+Variables:
+- a: An integer value, set to 10.
+- b: An integer value, set to 5.
+
+Functions:
+- add(a, b): Returns the sum of `a` and `b`.
+- sub(a, b): Returns the difference when `b` is subtracted from `a`.
+- mul(a, b): Returns the product of `a` and `b`.
+- div(a, b): Returns the quotient when `a` is divided by `b`.
+
+Logic:
+1. Import the necessary functions from the `calculator_1` module.
+2. Define the variables `a` and `b`.
+3. Perform the arithmetic operations using the imported functions and print the results in a formatted string.
+4. The `if __name__ == "__main__":` block is included to allow the script to be run as a standalone program.
+"""
+
 from calculator_1 import add, sub, mul, div
 
+# Define the variables
 a = 10
 b = 5
 
+# Perform arithmetic operations and print the results
 print("{} + {} = {}".format(a, b, add(a, b)))
 print("{} - {} = {}".format(a, b, sub(a, b)))
 print("{} * {} = {}".format(a, b, mul(a, b)))
 print("{} / {} = {}".format(a, b, div(a, b)))
 
+# Ensure the script can be run as a standalone program
 if __name__ == "__main__":
     pass
