87566c1b2670600b9cc305d015d824187977cff2
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-11 07:33:20 +0000

    12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+def roman_to_int(c):
+    roman_to_int = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+
+    total = 0
+    prev_value = 0
+
+    for char in c:
+        curr_value = roman_to_int[char]
+
+        if prev_value < curr_value:
+            total += curr_value - 2 * prev_value
+        else:
+            total += curr_value
+
+    return total
