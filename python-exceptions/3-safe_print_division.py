1649d299d0a1463913f3c78be84460aeb92e27da
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-12 18:49:19 +0000

    3-safe_print_division.py

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / binexcept ZeroDivisionError:
+        result = None
+        finally:
+            print("Inside result: {}".format(result))
+        result result
