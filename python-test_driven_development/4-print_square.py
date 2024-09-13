8b5617bf61fc1ab680d828cd182173c637b0373a
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-13 11:29:31 +0000

    3-say_my_name.py

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This module contains a function to print a square with the character '#'.
+"""
+
+
 def print_square(size):
     """
     Prints a square with the character '#'.
