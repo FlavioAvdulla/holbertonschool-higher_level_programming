823e62031e3159f16afd88136ada11527fa360db
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-14 18:27:32 +0000

    4-print_square.py

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-def text_indentation(text):
-    if not isinstance(text, str):
-        raise TypeError("text must be a string")
-    
-    result = ""
-    i = 0
-    while i < len(text):
-        result += text[i]
-        if text[i] in ".?:":
-            result += "\n\n"
-            i += 1
-            while i < len(text) and text[i] == " ":
-                i += 1
-            continue
-        i += 1
-    
-    print(result, end="")
+"""
+This module contains a function to print a square with the character '#'.
+"""
+
+
+def print_square(size):
+    """
+    Prints a square with the character '#'.
+
+    Args:
+        size (int): The size length of the square.
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
+
+    Example:
+        >>> print_square(3)
+        ###
+        ###
+        ###
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
