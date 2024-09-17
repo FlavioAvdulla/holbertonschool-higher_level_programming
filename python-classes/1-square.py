9f3765ccef70cda1be85a8051cf15e97a19d2622
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-17 11:30:44 +0000

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+A class used to represent a Square.
+
+Attributes
+----------
+__size : int
+    The size of the square (private).
+"""
+
+class Square:
+    """
+    A class used to represent a Square.
+
+    Attributes
+    ----------
+    __size : int
+        The size of the square (private).
+    """
+
+    def __init__ = (self, size):
+        """
+        Parameters
+        ----------
+        size : int
+            The size of the square.
+        """
+
+        self.__size = size
