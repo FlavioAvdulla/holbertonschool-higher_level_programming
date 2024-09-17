e772dc6d1577537f9a79bdd2482a22f619219abf
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-17 11:17:35 +0000

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 class Square:
+    """
+    This is an empty class that defines a square.
+
+    The class currently does not have any attributes or methods.
+    It serves as a placeholder for future development where
+    attributes and methods related to a square can be added.
+    """
     pass
