07f10f0cc11d26d9640cc055c94272f43091104d
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-18 10:29:52 +0000

    changes

--- python-classes/file6.py
+++ python-classes/file6.py
@@ -1,4 +1,22 @@
-person data = {
-    "name": "Flavio"
-    
-}
+class Bank_Account:
+    def __init__(self, account_holder, balance=0):
+        self.account_holder = account_holder
+        self.balance = balance
+
+    def deposit(self, amount):
+        if not isinstance(amount, int):
+            raise TypeError ("You should type only numbers")
+        self.balance += amount
+        return f"The added amount is: {amount}, new balance is {self.balance}."
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            return "Insufficient fund."
+        else:
+            self.balance -= amount
+            return f"You withdrew {amount}, your new balance is: {self.balance}"
+        print()
+account = Bank_Account("Flavio", 1400)
+
+print(account.deposit(80))
+print(account.withdraw(2000))
