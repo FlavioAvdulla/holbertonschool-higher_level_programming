2dd2a4af5a76a95d6e2f955ee577027f8314ae32
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-19 10:00:29 +0000

    0-rectangle.py

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
+"""
+Rectangle Module
+
+This module provides a simple Rectangle class.
+"""
+
+
 class Rectangle:
+    """
+    A class used to represent a Rectangle.
+
+    Attributes
+    ----------
+    None
+
+    Methods
+    -------
+    None
+    """
     pass
