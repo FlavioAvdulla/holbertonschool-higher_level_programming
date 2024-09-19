5bb9b9a56590107cd181f2901960074bb449e09a
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-19 09:49:34 +0000

    changes

--- python-classes/file8.py
+++ python-classes/file8.py
@@ -1,11 +1,2 @@
-class Car:
-    def __init__(self, name, model, year):
-        self.name = name
-        self.model = model
-        self.year = year
-
-    def start_engine(self):
-        return f"The engine of {self.model} made in {self.year} is running."
-
-my_car = Car("Toyota", "Auris", 2023)
-print(my_car.start_engine())
+class Account:
+    def __init__
