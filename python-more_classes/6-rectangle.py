3d73c2c0e83d66a9646064e4a6d9f216d5e006f7
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-19 17:21:26 +0000

    6-rectangle.py

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -124,10 +124,11 @@ class Rectangle:
         Prints a message when an instance of Rectangle is deleted.
         """
         print("Bye rectangle...")
-        
+
     def __del__(self):
         """
         Prints a message when an instance of Rectangle is deleted.
         """
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
 
