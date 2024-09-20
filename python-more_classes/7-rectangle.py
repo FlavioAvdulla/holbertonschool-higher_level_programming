80550160e1ea47dbe62c5c8b6245a3a95ecda733
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-20 09:12:56 +0000

    7-rectangle.py

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -125,5 +125,5 @@ class Rectangle:
         """
         Prints a message when an instance of Rectangle is deleted.
         """
-        print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
