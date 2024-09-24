6d2faa0be173a874a9c9a79c3be4ff3fbd7961b1
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-24 07:20:21 +0000

    8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 
-"""
-Module about a rectangle class
-"""
-
+"""Module about a rectangle class"""
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-class Geometry(BaseGeometry):
-    """
-    Class about a rectangle
-    """
+
+class Rectangle(BaseGeometry):
+    """Class about a rectangle"""
     def __init__(self, width, height):
         BaseGeometry.integer_validator(self, "width", width)
         BaseGeometry.integer_validator(self, "height", height)
