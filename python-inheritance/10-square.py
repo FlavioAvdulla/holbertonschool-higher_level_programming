20b81a27da30c0de08674917cb393c53bfef6515
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-24 07:35:21 +0000

    10-square.py

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,54 +1,17 @@
 #!/usr/bin/python3
-"""
-This module defines a Square class that inherits from BaseGeometry.
-
-Classes:
-    Square: Represents a square with methods to initialize and calculate its area.
-"""
+"""Module about a square"""
 
 
-BaseGeometry = __import__('9-rectangle').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
 
 class Square(Rectangle):
-    """
-    A class used to represent a Square, inheriting from Rectangle
-
-    ...
-
-    Attributes
-    ----------
-    __size : int
-        The size of the square's sides, must be a positive integer
-
-    Methods
-    -------
-    __init__(self, size):
-        Initializes the square with the given size after validation
-
-    area(self):
-        Calculates and returns the area of the square
-    """
-
+    """Class that implements a square"""
     def __init__(self, size):
-        """
-        Constructs all the necessary attributes for the square object.
-
-        Parameters
-        ----------
-        size : int
-            The size of the square's sides, must be a positive integer
-        """
-        self.integer_validator("size", size)
+        Rectangle.integer_validator(self, "size", size)
         super().__init__(size, size)
         self.__size = size
 
     def area(self):
-        """
-        Calculates the area of the square.
-
-        Returns
-        -------
-        int
-            The area of the square
-        """
+        """Func to calculate area"""
         return self.__size ** 2
