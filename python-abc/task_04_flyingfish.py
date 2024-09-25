0bc782d90f384820b4edbe9e6c68b8fd6352c00a
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-25 17:31:55 +0000

    task_04_flyingfish.py

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+FlyingFish demonstrates multiple inheritance by combining Fish and Bird behaviors.
+"""
+
+class Fish:
+    """Represents a fish with swimming ability."""
+    def swim(self):
+        print("The fish is swimming")
+
+    def habitat(self):
+        print("The fish lives in water")
+
+class Bird:
+    """Represents a bird with flying ability."""
+    def fly(self):
+        print("The bird is flying")
+
+    def habitat(self):
+        print("The bird lives in the sky")
+
+class FlyingFish(Fish, Bird):
+    """A fish that can also fly, inheriting from both Fish and Bird."""
+    def fly(self):
+        print("The flying fish is soaring!")
+
+    def swim(self):
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        print("The flying fish lives both in water and the sky!")
