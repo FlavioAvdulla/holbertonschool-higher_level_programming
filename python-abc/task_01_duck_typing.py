daa2e04fa39cee419c8ce694b79483d6660bc618
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-25 12:25:45 +0000

    task_01_duck_typing.py

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -63,9 +63,3 @@ def shape_info(shape):
     print(f"Perimeter: {shape.perimeter()}")
     print(f"Area: {shape.area()}")
 
-if __name__ == "__main__":
-    circle = Circle(radius=5)
-    rectangle = Rectangle(width=4, height=7)
-
-    shape_info(circle)
-    shape_info(rectangle)
