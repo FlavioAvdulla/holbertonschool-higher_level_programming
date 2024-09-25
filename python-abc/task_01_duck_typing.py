da2e53f87f315fe6ea57802782372d49cbfae714
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-25 12:48:16 +0000

    task_01_duck_typing.py

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -3,6 +3,7 @@
 This module defines abstract and concrete classes for different shapes.
 It includes methods to calculate the area and perimeter of the shapes.
 """
+
 from abc import ABC, abstractmethod
 import math
 
@@ -10,6 +11,13 @@ import math
 class Shape(ABC):
     """
     Abstract base class for shapes.
+    
+    Methods
+    -------
+    area() :
+        Calculate the area of the shape.
+    perimeter() :
+        Calculate the perimeter of the shape.
     """
 
     @abstractmethod
@@ -30,6 +38,18 @@ class Shape(ABC):
 class Circle(Shape):
     """
     Concrete class for a circle, inheriting from Shape.
+    
+    Attributes
+    ----------
+    radius : float
+        The radius of the circle.
+    
+    Methods
+    -------
+    area() :
+        Calculate the area of the circle.
+    perimeter() :
+        Calculate the perimeter of the circle.
     """
     def __init__(self, radius):
         self.radius = radius
@@ -44,6 +64,20 @@ class Circle(Shape):
 class Rectangle(Shape):
     """
     Concrete class for a rectangle, inheriting from Shape.
+    
+    Attributes
+    ----------
+    width : float
+        The width of the rectangle.
+    height : float
+        The height of the rectangle.
+    
+    Methods
+    -------
+    area() :
+        Calculate the area of the rectangle.
+    perimeter() :
+        Calculate the perimeter of the rectangle.
     """
     def __init__(self, width, height):
         self.width = width
@@ -59,6 +93,11 @@ class Rectangle(Shape):
 def shape_info(shape):
     """
     Print the perimeter and area of a shape.
+    
+    Parameters
+    ----------
+    shape : Shape
+        An instance of a class that inherits from Shape.
     """
     print(f"Perimeter: {shape.perimeter()}")
     print(f"Area: {shape.area()}")
