1c3c00f8a1670e5f611fc0f08ceff6ccb4fd06a0
Flavio Avdulla <a.flavio4366@gmail.com>
2024-10-01 08:15:15 +0000

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+File: read_file_module.py
+Author: Your Name
+Description: This module provides a function to read and print the contents of a text file.
+"""
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
     with open('filename', 'r', encoding='utf-8') as file:
