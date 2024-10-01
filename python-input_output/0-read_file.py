c188cc6ed7e159cccae0670a5d3afb5c1522e086
Flavio Avdulla <a.flavio4366@gmail.com>
2024-10-01 09:01:36 +0000

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
 File: 0-read_file.py
-Author: Your Name
-Description: This module provides a function to read and print the contents of a text file.
+Author: Flavio Avdulla
+Reads and prints the contents of a text file.
 """
 
 
@@ -11,7 +11,7 @@ def read_file(filename=""):
     Reads a text file (UTF8) and prints it to stdout.
 
     Args:
-        filename (str): The name of the file to read. Defaults to an empty string.
+        filename: The name of the file to read. Defaults to an empty string.
     """
     with open(filename, 'r', encoding='utf-8') as file:
         print(file.read())
