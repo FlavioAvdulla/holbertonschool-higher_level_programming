d86454f7538963036463294ff26f878998bbaf33
Flavio Avdulla <a.flavio4366@gmail.com>
2024-10-01 17:50:51 +0000

    10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -9,31 +9,10 @@ last name, and age.
 
 
 class Student:
-"""
-A class used to represent a Student.
-
-Attributes
-----------
-first_name : str
-    The first name of the student.
-last_name : str
-    The last name of the student.
-age : int
-    The age of the student.
-
-Methods
--------
-to_json(attrs=None):
-    Retrieves a dictionary representation of a
-    Student instance.
-"""
-
-def __init__(self, first_name, last_name, age):
     """
-    Constructs all the necessary attributes for the
-    student object.
+    A class used to represent a Student.
 
-    Parameters
+    Attributes
     ----------
     first_name : str
         The first name of the student.
@@ -41,32 +20,54 @@ def __init__(self, first_name, last_name, age):
         The last name of the student.
     age : int
         The age of the student.
-    """
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
 
-def to_json(self, attrs=None):
-    """
-    Retrieves a dictionary representation of a Student instance.
-
-    If attrs is a list of strings, only attribute names
-    contained in this list must be retrieved.
-    Otherwise, all attributes must be retrieved.
-
-    Parameters
-    ----------
-    attrs : list, optional
-        A list of attribute names to retrieve (default is None).
-
-    Returns
+    Methods
     -------
-    dict
-        A dictionary containing the student's attributes.
+    to_json(attrs=None):
+        Retrieves a dictionary representation of a
+        Student instance.
     """
-    if attrs is None:
-        return self.__dict__
-    else:
-        return {
-            attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)
-        }
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Constructs all the necessary attributes for the
+        student object.
+
+        Parameters
+        ----------
+        first_name : str
+            The first name of the student.
+        last_name : str
+            The last name of the student.
+        age : int
+            The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        If attrs is a list of strings, only attribute names
+        contained in this list must be retrieved.
+        Otherwise, all attributes must be retrieved.
+
+        Parameters
+        ----------
+        attrs : list, optional
+            A list of attribute names to retrieve (default is None).
+
+        Returns
+        -------
+        dict
+            A dictionary containing the student's attributes.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {
+                attr: getattr(self, attr) for attr in attrs
+                if hasattr(self, attr)
+            }
