2daa77dabd6027664974f6a850694c8546307d07
Flavio Avdulla <a.flavio4366@gmail.com>
2024-10-01 09:47:25 +0000

    2-append_write.py

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module provides a function to append text to a file.
+
+Functions:
+    append_write(filename="", text="") -> int:
+        Appends the given text to the specified file and returns the number of characters written.
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends the given text to the specified file.
+
+    Args:
+        file_append: The name of the file to append to. Defaults to an empty string.
+        file_append: The text to append to the file. Defaults to an empty string.
+    Returns:
+        int: The number of the characters written to the file.
+    """
+    with open(filename, 'a', encoding='utf-8') as file:
+        file.write(text)
+    return len(text)
