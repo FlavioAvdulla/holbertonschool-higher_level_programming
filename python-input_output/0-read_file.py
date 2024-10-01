0986a4aa3100128aa7d04770ff61e063c9fa9811
Flavio Avdulla <a.flavio4366@gmail.com>
2024-10-01 08:56:42 +0000

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -14,4 +14,4 @@ def read_file(filename=""):
         filename (str): The name of the file to read. Defaults to an empty string.
     """
     with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read().strip())
+        print(file.read())
