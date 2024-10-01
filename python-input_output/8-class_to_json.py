852ab0ffe955bc0d6e9825a7992dfeba609caefe
Flavio Avdulla <a.flavio4366@gmail.com>
2024-10-01 17:00:19 +0000

    8-class_to_json.py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -7,7 +7,7 @@ description with simple data structure
 
 def class_to_json(obj):
     """
-    Returns the dictionary description 
+    Returns the dictionary description
     with simple data structure for
     JSON serialization of an object.
     """
