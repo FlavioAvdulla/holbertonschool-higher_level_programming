34bb35c23b9758dc7b14c8fae17b813350d31117
Flavio Avdulla <a.flavio4366@gmail.com>
2024-10-01 17:48:14 +0000

    10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -9,10 +9,31 @@ last name, and age.
 
 
 class Student:
-    """
-    A class used to represent a Student.
+"""
+A class used to represent a Student.
 
-    Attributes
+Attributes
+----------
+first_name : str
+    The first name of the student.
+last_name : str
+    The last name of the student.
+age : int
+    The age of the student.
+
+Methods
+-------
+to_json(attrs=None):
+    Retrieves a dictionary representation of a
+    Student instance.
+"""
+
+def __init__(self, first_name, last_name, age):
+    """
+    Constructs all the necessary attributes for the
+    student object.
+
+    Parameters
     ----------
     first_name : str
         The first name of the student.
@@ -20,53 +41,32 @@ class Student:
         The last name of the student.
     age : int
         The age of the student.
-
-    Methods
-    -------
-    to_json(attrs=None):
-        Retrieves a dictionary representation of a
-        Student instance.
     """
+    self.first_name = first_name
+    self.last_name = last_name
+    self.age = age
 
-    def __init__(self, first_name, last_name, age):
-        """
-        Constructs all the necessary attributes for the
-        student object.
+def to_json(self, attrs=None):
+    """
+    Retrieves a dictionary representation of a Student instance.
 
-        Parameters
-        ----------
-        first_name : str
-            The first name of the student.
-        last_name : str
-            The last name of the student.
-        age : int
-            The age of the student.
-        """
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+    If attrs is a list of strings, only attribute names
+    contained in this list must be retrieved.
+    Otherwise, all attributes must be retrieved.
 
-    def to_json(self, attrs=None):
-        """
-        Retrieves a dictionary representation of a Student instance.
+    Parameters
+    ----------
+    attrs : list, optional
+        A list of attribute names to retrieve (default is None).
 
-        If attrs is a list of strings, only attribute names
-        contained in this list must be retrieved.
-        Otherwise, all attributes must be retrieved.
-
-        Parameters
-        ----------
-        attrs : list, optional
-            A list of attribute names to retrieve (default is None).
-
-        Returns
-        -------
-        dict
-            A dictionary containing the student's attributes.
-        """
-        if attrs is None:
-            return self.__dict__
-        else:
-            return {
-                attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)
-            }
+    Returns
+    -------
+    dict
+        A dictionary containing the student's attributes.
+    """
+    if attrs is None:
+        return self.__dict__
+    else:
+        return {
+            attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)
+        }
