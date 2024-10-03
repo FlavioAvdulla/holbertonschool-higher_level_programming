5d5c603f5686d4dcded22ab9f2d39988bbf68591
Flavio Avdulla <a.flavio4366@gmail.com>
2024-10-03 10:52:38 +0000

    task_00_basic_serialization.py

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -12,15 +12,18 @@ data_to_serialize = {
     "city": "New York"
 }
 
+
 def serialize_and_save_to_file(data, filename):
     with open('filename', 'w') as file:
         json.dump(data, file)
     print(f"Data serialised and saved to: {filename}.")
 
+
 def load_and_deserialize(filename):
     with open('filename', 'r') as file:
         data = json.load(file)
     return data
+
 
 serialize_and_save_to_file(data_to_serialize, 'data.json')
 load_and_deserialize('data.json')
