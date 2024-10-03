92c4023a99c0aa0471ecccf8327d531400534680
Flavio Avdulla <a.flavio4366@gmail.com>
2024-10-03 10:54:02 +0000

    task_00_basic_serialization.py

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-"""Module for JSON serialization and deserialization."""
+"""
+Module for JSON serialization and deserialization.
+
+Functions:
+- serialize_and_save_to_file(data, filename):
+Serialize a dictionary to a JSON file.
+- load_and_deserialize(filename): Deserialize a
+JSON file to a dictionary.
+"""
 
 
 import json
@@ -12,7 +20,24 @@ data_to_serialize = {
 
 
 def serialize_and_save_to_file(data, filename):
+    """
+    Serialize a Python dictionary to a JSON file.
+
+    Parameters:
+    data (dict): The Python dictionary to serialize.
+    filename (str): The filename of the output
+    JSON file. If the file exists, it will be replaced.
+    """
     with open('filename', 'w') as file:
+        """
+    Load and deserialize a JSON file to a Python dictionary.
+
+    Parameters:
+    filename (str): The filename of the input JSON file.
+
+    Returns:
+    dict: The deserialized Python dictionary.
+    """
         json.dump(data, file)
     print(f"Data serialised and saved to: {filename}.")
 
@@ -21,7 +46,3 @@ def load_and_deserialize(filename):
     with open('filename', 'r') as file:
         data = json.load(file)
     return data
-
-
-serialize_and_save_to_file(data_to_serialize, 'data.json')
-load_and_deserialize('data.json')
