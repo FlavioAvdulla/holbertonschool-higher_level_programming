90c4bfa04f3dbb3535f837c025b43b80b45a6a4c
Flavio Avdulla <a.flavio4366@gmail.com>
2024-10-08 07:03:27 +0000

    task_02_requests.py

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""
+Script: Fetch and Save Posts
+
+Description:
+Fetches posts from a public API, prints titles, and saves posts to a CSV file.
+
+Dependencies:
+- requests: HTTP library for Python.
+- csv: Module for handling CSV files.
+
+Functions:
+- fetch_and_print_posts(): Prints post titles.
+- fetch_and_save_posts(): Saves posts to a CSV file.
+"""
+
+import requests
+import csv
+
+def fetch_and_print_posts():
+    """Fetches posts and prints titles."""
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    if response.status_code == 200:
+        for post in response.json():
+            print(post['title'])
+
+def fetch_and_save_posts():
+    """Fetches posts and saves them to a CSV file."""
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    if response.status_code == 200:
+        with open('posts.csv', 'w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
+            writer.writeheader()
+            writer.writerows(response.json())
+
+if __name__ == "__main__":
+    fetch_and_print_posts()
+    fetch_and_save_posts()
