62fe04ce942b24b5c2c460961190ecf9fffe738d
Flavio Avdulla <a.flavio4366@gmail.com>
2024-10-10 08:19:04 +0000

    task_04_flask.py

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,52 +1,29 @@
 #!/usr/bin/python3
-"""
-Flask API Example
-This script sets up a simple Flask API with
-multiple endpoints to manage user data.
-"""
 
 from flask import Flask, jsonify, request
 
-
 app = Flask(__name__)
-
 
 users = {}
 
 
 @app.route('/')
 def home():
-    """
-    Root endpoint returns a welcome message.
-    """
     return "Welcome to the Flask API!"
 
 
 @app.route('/status')
 def status():
-    """
-    Status endpoint returns OK to indicate the
-    API is running.
-    """
     return "OK"
 
 
 @app.route('/data')
 def data():
-    """
-    Data endpoint returns a list of all usernames stored in the API.
-    """
     return jsonify(list(users.keys()))
 
 
 @app.route('/users/<username>')
 def get_user(username):
-    """
-    User endpoint returns the full object
-    corresponding to the provided username.
-    If the user does not exist, returns an
-    error message.
-    """
     user = users.get(username)
     if user:
         return jsonify(user)
@@ -56,14 +33,6 @@ def get_user(username):
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
-    """
-    Add User endpoint accepts POST requests to
-    add a new user.
-    Parses the incoming JSON data and adds the
-    new user to the users dictionary.
-    Returns a confirmation message with the
-    added user’s data.
-    """
     new_user = request.get_json()
     username = new_user.get('username')
     if not username:
@@ -71,7 +40,7 @@ def add_user():
     if username in users:
         return jsonify({"error": "Username already exists"}), 400
     users[username] = new_user
-    return jsonify({"message": "User added", "user": new_user})
+    return jsonify({"message": "User added", "user": new_user}), 201
 
 
 if __name__ == "__main__":
