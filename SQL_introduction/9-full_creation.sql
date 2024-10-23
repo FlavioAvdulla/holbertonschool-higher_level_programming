a388904f92783a4cf8136afbd3de57b5799d79a4
Flavio Avdulla <a.flavio4366@gmail.com>
2024-10-23 07:35:47 +0000

    9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,11 +4,11 @@ CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
-)
+);
 
--- Create table if it doesn't exist
+-- Insert multiple rows
 
-INSERT INTO (id, name, score) VALUES (1, 'John', 10)
-INSERT INTO (id, name, score) VALUES (2, 'Alex', 3)
-INSERT INTO (id, name, score) VALUES (3, 'Bob', 14)
-INSERT INTO (id, name, score) VALUES (4, 'George', 8)
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
