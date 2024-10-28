832174666e6c03230baa63fc3e75cd97166b2f44
Flavio Avdulla <a.flavio4366@gmail.com>
2024-10-28 09:38:33 +0000

    0-privileges.sql

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- script that list all priviledges FOR USER 'user_0d_1'
+SHOW GRANTS FOR 'user_0d_1' @ 'localhost';
+-- script that list all priviledges FOR USER 'user_0d_2'
+SHOW GRANTS FOR 'user_0d_2' @ 'localhost';
