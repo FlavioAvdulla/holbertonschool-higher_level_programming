573b815e5d490f5ca8e1d04777d72acab398e968
Flavio Avdulla <a.flavio4366@gmail.com>
2024-10-28 09:56:03 +0000

    2-create_read_user.sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -6,6 +6,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
--- Write a script that gives the user user_0d_2 read priviledges.
+-- Write a script that gives the user user_0d_2 read privilege
 
-GRANT SELECT ON hbtn_0d_2* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
