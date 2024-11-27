3d9915f80d87c45b38a49b8b07ea930c021eeea9
Flavio Avdulla <a.flavio4366@gmail.com>
2024-11-27 07:41:24 +0000

    5-script.js

--- javascript-dom_manipulation/5-script.js
+++ javascript-dom_manipulation/5-script.js
@@ -1,10 +1,5 @@
-// Find the element header
-let header = document.querySelector("header");
-
-// Find element with id update_header
-let update_header = document.getElementById("update_header");
-
-// add event listener for header update
-update_header.addEventListener("click", () => {
-    header.innerHTML = "New Header!!!";
-})
+const update = document.getElementById("update_header");
+update.addEventListener("click", function () {
+    const header = document.querySelector("header");
+    header.textContent = "New Header!!!";
+});
